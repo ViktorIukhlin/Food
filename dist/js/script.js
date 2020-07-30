877cb4aca9fe40ff2e04c6c703e188464bca1af2
Viktor Iukhlin <iukhlin.v@gmail.com>
2020-07-30 21:42:02 +0300

    ++

--- dist/js/script.js
+++ dist/js/script.js
@@ -128,7 +128,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    //const modalTimerId = setTimeout(openModal, 10000);
+    const modalTimerId = setTimeout(openModal, 60000);
 
     function showModalByScroll() {
         if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
